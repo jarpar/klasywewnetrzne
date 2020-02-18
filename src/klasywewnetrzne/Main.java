086761d824d70b9082d2aadeab679a8fc7114bbe
jarpar <partyka.jaroslaw@gmail.com>
2020-02-18 16:10:52 +0100

    Initial commit

---
+++ src/klasywewnetrzne/Main.java
@@ -0,0 +1,16 @@
+package klasywewnetrzne;
+
+public class Main {
+    public static void main(String[] args) {
+        KontoBankowe kontoBankowe = new KontoBankowe();
+    }
+}
+
+class KontoBankowe {
+
+    private double stanKonta;
+
+    double getStanKonta() {
+        return this.stanKonta;
+    }
+}
