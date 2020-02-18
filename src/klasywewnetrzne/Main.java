3d1e626f6481d4f751da74c1d1a5471a5e99a88c
jarpar <partyka.jaroslaw@gmail.com>
2020-02-18 16:20:07 +0100

    refactored

--- src/klasywewnetrzne/Main.java
+++ src/klasywewnetrzne/Main.java
@@ -2,15 +2,38 @@ package klasywewnetrzne;
 
 public class Main {
     public static void main(String[] args) {
-        KontoBankowe kontoBankowe = new KontoBankowe();
+        KontoBankowe kontoBankowe = new KontoBankowe(1000);
+        kontoBankowe.start(5);
     }
 }
 
 class KontoBankowe {
+    public KontoBankowe(double stanKonta) {
+        this.stanKonta = stanKonta;
+    }
 
     private double stanKonta;
 
     double getStanKonta() {
         return this.stanKonta;
+    }
+
+    void start(double stopa) {
+        Odsetki odsetki = new Odsetki(stopa);
+        odsetki.zmienStanKonta(stopa);
+    }
+
+    class Odsetki {
+        private double stopaProcentowa;
+
+        public Odsetki(double stopaProcentowa) {
+            this.stopaProcentowa = stopaProcentowa;
+            this.zmienStanKonta(stopaProcentowa);
+        }
+
+        void zmienStanKonta(double stopa) {
+            double odsetki = (stanKonta * stopa) / 100;
+            stanKonta += odsetki;
+        }
     }
 }
